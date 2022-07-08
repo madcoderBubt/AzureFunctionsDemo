83129c5af344047b5d1334b240f8ba8c831acdf5
madcoderbubt <sudhanandabiswas@yahoo.com>
2022-07-09 02:28:20 +0600

    Azure Function Publish & ci/cd pipeline added

---
+++ AzureFunctionsDemo/GitMonitorApp.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunctionsDemo
+{
+    public static class GitMonitorApp
+    {
+        [FunctionName("GitMonitorApp")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Git Monitor App");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            log.LogInformation(requestBody);
+
+            return new OkResult();
+        }
+    }
+}
